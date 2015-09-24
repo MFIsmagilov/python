f170c3927b8c027e1ec6057884b8632080cecfde
Marat-FI <mf.ismagilov@gmail.com>
2015-09-24 14:46:45 +0400

    e^x
    e^x

---
+++ exp/exp.py
@@ -0,0 +1,16 @@
+﻿import math
+x = -5.1;
+eps = 0.000000000000001;
+lastSum = 1.0;
+result = 1.0;
+
+n = 1;
+
+while abs(lastSum) >= eps :
+    lastSum = lastSum * x / n;
+    result += lastSum;
+    n += 1;
+    
+print 'e^',x, " = ", result; 
+
+print 'e^',x, " = ", math.exp( x ); 
